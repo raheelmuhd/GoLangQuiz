275467066ec3ee452add87ba3804ca68649277e7
Muhammad Raheel <115684857+raheelmuhd@users.noreply.github.com>
2024-02-29 22:33:59 +0500

    Create blockchain.go in main

---
+++ blockchain.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Block struct {
+	Timestamp     int64
+	Data          string
+	PreviousBlock *Block
+}
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+func NewBlock(data string, previousBlock *Block) *Block {
+	return &Block{Timestamp: time.Now().Unix(), Data: data, PreviousBlock: previousBlock}
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	previousBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, previousBlock)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+func NewBlockchain() *Blockchain {
+	genesisBlock := NewBlock("Genesis Block", nil)
+	return &Blockchain{blocks: []*Block{genesisBlock}}
+}
+
+func (bc *Blockchain) DisplayAllBlocks() {
+	for _, block := range bc.blocks {
+		fmt.Printf("Timestamp: %d, Data: %s\n", block.Timestamp, block.Data)
+	}
+}
+
+func main() {
+	bc := NewBlockchain()
+	bc.AddBlock("Block 1")
+	bc.AddBlock("Block 2")
+	bc.DisplayAllBlocks()
+}
